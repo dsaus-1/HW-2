f97af41537cb56025e4f5d58f0ac7d4f2a55b6ff
dsaush-1 <saushkin.reg@gmail.com>
2023-01-11 18:23:59 +0300

    добавил комменты к заданиям

--- hw-48-1-2.sql
+++ hw-48-1-2.sql
@@ -1,12 +1,17 @@
+--"имя контакта" и "город" (contact_name, country) из таблицы customers (только эти две колонки)
 SELECT contact_name, country FROM customers;
 
+--идентификатор заказа и разницу между датами формирования (order_date) заказа и его отгрузкой (shipped_date) из таблицы orders
 SELECT order_id, shipped_date - order_date AS date_difference
 FROM orders;
 
+--все города без повторов, в которых зарегестрированы заказчики (customers)
 SELECT DISTINCT city FROM customers;
 
+--количество заказов (таблица orders)
 SELECT COUNT(1) FROM orders;
 
+--количество стран, в которые откружался товар (таблица orders, колонка ship_country)
 SELECT COUNT(DISTINCT ship_country) FROM orders;
 
 
